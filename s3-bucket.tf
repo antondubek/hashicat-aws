de9bf9c432093a9533901f5b36a753d5e5f4ab37
antondubek <anthony.makepeace@bt.com>
2021-10-07 11:52:03 +0000

    Added s3 bucket module - new new

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/antondubek/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = var.prefix
+  bucket = "yolo-swaggins-hashicorp"
   acl    = "private"
 
   versioning = {
